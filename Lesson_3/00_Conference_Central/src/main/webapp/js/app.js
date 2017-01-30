de8cc206746cc39a6a5d2638ec0b3410f6d3ef16
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2017-01-30 14:39:08 +0200

    solved lesson 3

--- Lesson_3/00_Conference_Central/src/main/webapp/js/app.js
+++ Lesson_3/00_Conference_Central/src/main/webapp/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'replace with your client id',
+        CLIENT_ID: '681059066348-975gdpp1p7466gbo3u1te1am9vde05g9.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
         signedIn: false
     };
